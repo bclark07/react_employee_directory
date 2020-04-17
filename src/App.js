c3d23325d8fee295f8a00d64c45e8cccc211ba17
Brittany Clark <clark.brittany.j@gmail.com>
2020-04-16 22:12:42 -0400

    search functioning

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import EmployeeCard from "./components/EmployeeCard";
 
 function App() {
   const [employeeData, setEmployeeData] = useState([]);
+  const [searchName, setsearchName] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,18 +20,18 @@ function App() {
 
   return (
     <div className="container">
-      <div class="jumbotron jumbotron-fluid bg-info">
-        <div class="container">
-          <h1 class="display-4 text-center">Employee Directory</h1>
-          <p class="lead  text-center">
+      <div className="jumbotron jumbotron-fluid bg-info">
+        <div className="container">
+          <h1 className="display-4 text-center">Employee Directory</h1>
+          <p className="lead  text-center">
             Click on cards to filter by heading or use the search box to narrow
             your results.
           </p>
         </div>
       </div>
-      <Search />
+      <Search setsearchName={setsearchName} searchName={searchName} />
       {employeeData && (
-        <table class="table mb-2 mt-sm-2">
+        <table className="table mb-2 mt-sm-2">
           <thead>
             <tr>
               <th scope="col">Image</th>
@@ -41,9 +42,25 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {employeeData.slice(0, 15).map((employee) => (
-              <EmployeeCard key={employee.id.value} employee={employee} />
-            ))}
+            {searchName &&
+              employeeData
+                .filter((employee) => {
+                  const regex = new RegExp(`${searchName}`, "gi"); //match wether upper or lower case
+                  return employee.name.first.match(regex);
+                })
+                // .filter((employee) => employee.name.first === searchName)
+                .map((filteredList) => (
+                  <EmployeeCard
+                    key={filteredList.id.value}
+                    employee={filteredList}
+                  />
+                ))}
+            {!searchName &&
+              employeeData
+                .slice(0, 15)
+                .map((employee) => (
+                  <EmployeeCard key={employee.id.value} employee={employee} />
+                ))}
           </tbody>
         </table>
       )}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Search(props) {
-  const [searchName, setsearchName] = useState("");
+function Search({ setsearchName, searchName }) {
+  //replaced with e.target.value
+  // const setsearchName = (event) => {
+  //   setsearchName(event.target.value);
+  // };
 
- const handleInputChange = (event) => {
-    setsearchName(event.target.value);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    console.log(searchName);
-  };
+  // const handleFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(searchName);
+  // };
 
   return (
     <form>
       <div className="form-inline justify-content-center">
         <label htmlFor="search">Search a Name: </label>
         <input
-          onChange={handleInputChange}
+          onChange={(e) => setsearchName(e.target.value)}
           value={searchName}
           name="search"
           type="text"
           className="form-control mb-2 mx-sm-2"
           id="search"
         />
-        <button onClick={handleFormSubmit} className="btn btn-secondary mb-2 ">
+        {/* <button onClick={handleFormSubmit} className="btn btn-secondary mb-2 ">
           Search
-        </button>
+        </button> */}
       </div>
     </form>
   );
